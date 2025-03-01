139d394a51b107e1e0f9c4f2577b6ccf8da8f9d9
Saif Lakhani <saiflakhani@gmail.com>
2025-03-01 15:55:36 -0800

    Added some game logic

--- dist/model/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CHOICE_SELECTED_KEY = exports.USER_ENTER_KEY = exports.HOST_READY_KEY = exports.GAME_OBJECT_KEY = exports.CHAT_MESSAGE_KEY = void 0;
-exports.CHAT_MESSAGE_KEY = "chat-message";
-exports.GAME_OBJECT_KEY = "game-object";
-exports.HOST_READY_KEY = "host-ready";
-exports.USER_ENTER_KEY = "user-enter";
-exports.CHOICE_SELECTED_KEY = 'choice-selected';
